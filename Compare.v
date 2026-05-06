8bdf08221f9a370ce57f03ecb40d8b9f22b8e7f7
tarafaa <tarafaa@outlook.com>
2026-05-06 11:11:11 -0700

    logic update for Compare

--- Compare.v
+++ Compare.v
@@ -1,35 +1,36 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-// Comparing if B is a negative number
-//if((i-k)>=0&&(i-k)<sizeB){
-//sum=sum+A_array[k]*B_array[i-k];
-
-//}
-//}
-//C_array[i]=sum;
-//}
 
 // input/output declaration
 module Compare (
-B_i // B index
+B[i] // B index
 k 
+B[n]
+B_total
 );
-entity Compare is
-    Port (
-        CODE_W : in  STD_LOGIC_VECTOR(3 downto 0);
-    );
-end Compare;
 
+main function: 
+process( CLK, RST)
 
-architecture Behavioral of Compare is
-signal synd_int : STD_LOGIC_VECTOR(3 downto 0);
+if (RST = 1) // asynchronous reset
+B[i] >= 0 then 
+k + 1 
+i - k = B[n]
 
-begin
+work.entity B_accumulate port map( // this will be a separate module to create a sum
+B_total <= B[n];
 
-    -- compare
-    synd_int <= CODE_W xor CODE_G;
-    SYND <= synd_int;
-    -- detects error/ if ERR = 1, error detected
-    ERR <= synd_int(0) or synd_int(1) or synd_int(2) or synd_int(3);
+)
 
+else
+
+halt; 
+
+end if;
+
+end process;
 end Behavioral;
+
+// FSM example 
+
+https://www.chipverify.com/verilog/verilog-fsm
