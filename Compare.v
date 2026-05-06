f78e27a1a0517c19fd696e18d8b2c87a42fa2f70
tarafaa <tarafaa@outlook.com>
2026-05-06 12:53:55 -0700

    Update Compare.v
    initial pseudocode for Compare module 
    - still need to make memory Module 
    - fix syntax

--- Compare.v
+++ Compare.v
@@ -1,36 +1,45 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+`timescale 1ns / 1ps
 
-// input/output declaration
-module Compare (
-B[i] // B index
-k 
-B[n]
-B_total
+
+
+module CompareB(
+    input [15:0] Bi, // Bi is the index of array B (impluse)
+    input k, // counter k 
+    input clk,
+    input rst,
+    output [15:0] Bn, 
+    output [15:0] Btotal
+    );
+endmodule
+
+wire kstate;
+wire clk;
+wire rst; 
+wire Bn;
+// at positive edge of the clock change the next state of k
+// asynchronous reset
+always @(posedge clk)  begin
+
+if (!rst) begin 
+Bi > 0 then 
+for(k = 0, k > 0, k++){
+kstate <= k + 1; // let n be the index value of k 
+if (k - B(i) > 0){ // if k - B(index value) is less 
+Bn <= B(i) - k(n); // calcualte the i - k 
+
+
+module Bmemory ( 
+.Bin(Bn),
+.Bout(Btotal)
 );
 
-main function: 
-process( CLK, RST)
+end module;
+}
+else 
 
-if (RST = 1) // asynchronous reset
-B[i] >= 0 then 
-k + 1 
-i - k = B[n]
+disable loop;
+}
 
-work.entity B_accumulate port map( // this will be a separate module to create a sum
-B_total <= B[n];
+end;
 
-)
-
-else
-
-halt; 
-
-end if;
-
-end process;
-end Behavioral;
-
-// FSM example 
-
-https://www.chipverify.com/verilog/verilog-fsm
+// not done but pseduocode  
