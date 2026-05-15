706aa658ca199b20a7e176ef124b47a611a25029
tarafaa <tarafaa@outlook.com>
2026-05-15 12:08:55 -0700

    Update MemoryA.v

--- MemoryA.v
+++ MemoryA.v
@@ -5,19 +5,31 @@ module MemoryA(
     input [4:0] addr,
     input WE,
     input clk,
+    input rst,
     output reg [15:0] Aout
-    );
+);
+
+    reg [15:0] A [31:0];
+
+    integer i;
+// async rst is included
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            Aout <= 16'd0;
+
     
-    reg[15:0] A [31:0];
-    
-    always @(posedge clk) begin
-        if (WE == 1'b1) begin
-            A[addr] <= Ain;
+            for (i = 0; i < 32; i = i + 1) begin
+                A[i] <= 16'd0;
+            end
         end
-        
         else begin
-            Aout <= A[addr];
+            if (WE == 1'b1) begin
+                A[addr] <= Ain;
+            end
+            else begin
+                Aout <= A[addr];
+            end
         end
     end
-    
+
 endmodule
