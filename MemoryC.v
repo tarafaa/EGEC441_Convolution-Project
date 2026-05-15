9d56cd2dda89473a5d74293d1d190fb63a386d7e
tarafaa <tarafaa@outlook.com>
2026-05-15 12:09:24 -0700

    Update MemoryC.v

--- MemoryC.v
+++ MemoryC.v
@@ -5,18 +5,29 @@ module MemoryC(
     input [4:0] addr,
     input WE,
     input clk,
+    input rst,
     output reg [31:0] Cout
-    );
-    
-    reg [31:0] C [31:0];
-    
-    always @(posedge clk) begin
-        if (WE == 1'b1) begin
-            C[addr] <= Cin;
+);
+
+    reg [31:0] C [0:31];
+
+    integer i;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            Cout <= 32'd0;
+
+            // async included
+            for (i = 0; i < 32; i = i + 1) begin
+                C[i] <= 32'd0;
+            end
         end
         else begin
-            Cout <= C[addr];
+            if (WE)
+                C[addr] <= Cin;
+            else
+                Cout <= C[addr];
         end
     end
-    
+
 endmodule
